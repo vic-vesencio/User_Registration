dd4c1df5fed31c4036b2e36178ec70f7e7a82d57
smitethee <ervicjohn.vesencio@gmail.com>
2021-08-21 16:11:02 +0800

    Added password visibility option

--- client/src/MainPage/Users/CreateUser.js
+++ client/src/MainPage/Users/CreateUser.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useContext, useState} from 'react';
 
 import { 
    IconButton, 
@@ -6,14 +6,20 @@ import {
    TextField,
    Modal,
    Backdrop,
-   Fade
+   Fade,
+   Button
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 //icons
 import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+import VisibilityOffIcon from '@material-ui/icons/VisibilityOff';
+import VisibilityIcon from '@material-ui/icons/Visibility';
 // import DeleteIcon from '@material-ui/icons/Delete';
 // import EditIcon from '@material-ui/icons/Edit';
+
+//context
+import UserContext from '../../Context/UserContext';
 
 const useStyles = makeStyles((theme) => ({
    modal: {
@@ -26,7 +32,7 @@ const useStyles = makeStyles((theme) => ({
       border: '2px solid #000',
       boxShadow: theme.shadows[5],
       padding: theme.spacing(2, 4, 3),
-      width: '50vw'
+      width: '40vw'
    },
    input: {
       width: '100%',
@@ -35,7 +41,15 @@ const useStyles = makeStyles((theme) => ({
 
 const CreateUser = () => {
    const classes = useStyles();
+   const newUserContext = useContext(UserContext);
    const [open, setOpen] = React.useState(false);
+   const [username, setUsername] = useState('');
+   const [password, setPassword] = useState('');
+   const [passVisible, setPassVisible] = useState(false);
+   const [firstName, setFirstName] = useState('');
+   const [lastName, setLastName] = useState('');
+   const [middleName, setMiddleName] = useState('');
+   const [email, setEmail] = useState('');
  
    const handleOpen = () => {
      setOpen(true);
@@ -44,6 +58,13 @@ const CreateUser = () => {
    const handleClose = () => {
      setOpen(false);
    };
+
+   const createUser = (e) =>{
+      e.preventDefault();
+      if(username !== '' && password !== '' && firstName !== '' && lastName !== '' && middleName !== '' && email !== ''){
+         alert('okay');
+      }
+   }
  
    return (
       <div>
@@ -63,14 +84,80 @@ const CreateUser = () => {
             }}
          >
             <Fade in={open}>
-            <div className={classes.paper}>
-               <h2>Create new user</h2>
-               <TextField
-                  variant="outlined"
-                  margin="dense"
-                  placeholder="username"
-               />
-            </div>
+               <div className={classes.paper}>
+                  <form onSubmit={(e)=>createUser(e)}>
+                     <h2>Create new user</h2>
+                     <TextField
+                        required
+                        className={classes.input}
+                        value={username}
+                        onChange={e=>{
+                           let value = (e.target.value).slice(0,12);
+                           setUsername(value);
+                        }}
+                        variant="outlined"
+                        margin="dense"
+                        placeholder="username"
+                     />
+                     <TextField
+                        required
+                        type={passVisible ? 'text': 'password'}
+                        style={{width: '90%'}}
+                        value={password}
+                        onChange={e=>{
+                           let value = (e.target.value).slice(0,12);
+                           setPassword(value);
+                        }}
+                        variant="outlined"
+                        margin="dense"
+                        placeholder="password"
+                     />
+                     <Tooltip title='see password?'>
+                        <IconButton style={{width: '10%'}} onClick={()=>setPassVisible(!passVisible)}>
+                           {passVisible ? <VisibilityIcon /> : <VisibilityOffIcon />}
+                        </IconButton>
+                     </Tooltip>
+                     <TextField
+                        required
+                        className={classes.input}
+                        value={firstName}
+                        onChange={e=>setFirstName(e.target.value)}
+                        variant="outlined"
+                        margin="dense"
+                        placeholder="first name"
+                     />
+                     <TextField
+                        required
+                        className={classes.input}
+                        value={middleName}
+                        onChange={e=>setMiddleName(e.target.value)}
+                        variant="outlined"
+                        margin="dense"
+                        placeholder="middle name"
+                     />
+                     <TextField
+                        required
+                        className={classes.input}
+                        value={lastName}
+                        onChange={e=>setLastName(e.target.value)}
+                        variant="outlined"
+                        margin="dense"
+                        placeholder="last name"
+                     />
+                     <TextField
+                        required
+                        type='email'
+                        className={classes.input}
+                        value={email}
+                        onChange={e=>setEmail(e.target.value)}
+                        variant="outlined"
+                        margin="dense"
+                        placeholder="email"
+                     />
+                     <Button type='submit' color="secondary">Create</Button>
+                     <Button type='button' color="secondary" onClick={()=>handleClose()}>Close</Button>
+                  </form>
+               </div>
             </Fade>
          </Modal>
       </div>
