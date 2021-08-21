78e3845d95814dccefde9923105f490dc5bdac9c
smitethee <ervicjohn.vesencio@gmail.com>
2021-08-21 13:31:08 +0800

    User model created

---
+++ models/User.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//Create Schema
+const UserSchema = new Schema({
+	username:{
+		type: String,
+		required: true,
+		unique: true
+   },
+   password:{
+		type: String,
+        required: true,
+	},
+	firstName:{
+		type: String,
+		required: true,
+	},
+	lastName:{
+		type: String,
+		required: true,
+	},
+	middleName:{
+		type: String,a
+		required: true,
+	},
+	email:{
+		type: String,
+		unique: true,
+		required: true
+	},
+	email:{
+		type: String,
+		unique: true,
+		required: true
+	},
+	dateCreated:{
+		type: Date,
+		default: Date.now
+	}
+});
+
+module.exports = User = mongoose.model('user', UserSchema);
